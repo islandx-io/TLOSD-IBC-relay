bfb12d0c51fc645d0e12ab773bb0b5e035f5e65d
Andreas <andreas@mytribez.com>
2020-07-30 12:32:56 +0200

    add main endpoits

--- reporter/src/eos/networks.ts
+++ reporter/src/eos/networks.ts
@@ -85,11 +85,11 @@ const TelosTestNetwork = createNetwork(
   `1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f`
 );
 const EosNetwork = createNetwork(
-  process.env.EOS_ENDPOINT,
+  process.env.EOS_ENDPOINT || `https://api.telos.africa:443`,
   `aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906`
 );
 const TelosNetwork = createNetwork(
-  process.env.TELOS_ENDPOINT,
+  process.env.TELOS_ENDPOINT || `https://api.telos.africa:443`,
   `4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11`
 );
 
